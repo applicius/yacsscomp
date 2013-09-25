59dcda6c987cf1feb0da54e687d5f777391e60f0
cchantep <chantepie@altern.org>
2013-09-25 19:21:26 +0200

    Remove useless code

--- yacsscomp.js
+++ yacsscomp.js
@@ -16,17 +16,16 @@ $.fn.stylableRadio = function() {
 
 $.fn.stylableCheckbox = function() {
   $(this).each(function() {
-    var e = $(this); var i = e.attr("for"); var c = $("#" + i); 
+    var e = $(this); var c = $("#" + e.attr("for"));
     e.html("<span class=\"checkbox-before-label\"></span><span class=\"wrapper\">" + e.html() + "</span><span class=\"checkbox-after-label\"></span>");
-    c.css({ 
-       visibility: "visible", height: "1px", width: "1px", margin: "-1px", 
-       position: "absolute", border: "0", padding: "0", clip: "rect(0 0 0 0)", 
+    c.css({
+       visibility: "visible", height: "1px", width: "1px", margin: "-1px",
+       position: "absolute", border: "0", padding: "0", clip: "rect(0 0 0 0)",
        overflow: "hidden"
     });
     e.click(function() {
-      var l = $("label[for='" + c.attr("id") + "']", c.parent());
-      if (l.hasClass("checkbox-checked")) l.removeClass("checkbox-checked")
-      else l.addClass("checkbox-checked")
+      if (e.hasClass("checkbox-checked")) e.removeClass("checkbox-checked")
+      else e.addClass("checkbox-checked")
     })
   })
 }
