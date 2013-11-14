e621c00d578f926e6bcee301b83961a979b42159
cchantep <chantepie@altern.org>
2013-11-14 19:51:43 +0100

    Avoid double init

--- yacsscomp.js
+++ yacsscomp.js
@@ -23,8 +23,12 @@
         // ---
 
         $(this).each(function() {
-            var e = $(this); var i = e.attr("for"); var r = $("#" + i); 
-            e.html("<span class=\"radio-before-label\"></span><span class=\"wrapper\">" + e.html() + "</span><span class=\"radio-after-label\"></span>");
+            var e = $(this); 
+
+            if (e.hasClass("yacsscomp")) { /* Skip */ return false }
+
+            var i = e.attr("for"), r = $("#" + i); 
+            e.addClass("yacsscomp").html("<span class=\"radio-before-label\"></span><span class=\"wrapper\">" + e.html() + "</span><span class=\"radio-after-label\"></span>");
             r.css({ 
                 visibility: "visible", height: "1px", width: "1px", 
                 margin: "-1px", position: "absolute", border: "0", 
@@ -38,8 +42,12 @@
 
     $.fn.stylableCheckbox = function() {
         $(this).each(function() {
-            var e = $(this); var c = $("#" + e.attr("for"));
-            e.html("<span class=\"checkbox-before-label\"></span><span class=\"wrapper\">" + e.html() + "</span><span class=\"checkbox-after-label\"></span>");
+            var e = $(this); 
+            
+            if (e.hasClass("yacsscomp")) { /* Skip */ return false }
+            
+            var c = $("#" + e.attr("for"));
+            e.addClass("yacsscomp").html("<span class=\"checkbox-before-label\"></span><span class=\"wrapper\">" + e.html() + "</span><span class=\"checkbox-after-label\"></span>");
             c.css({
                 visibility: "visible", height: "1px", width: "1px", margin: "-1px",
                 position: "absolute", border: "0", padding: "0", clip: "rect(0 0 0 0)",
